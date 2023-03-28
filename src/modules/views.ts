ed1830bd49d8141dd6412f6c20a99e3ef131c25b
MuiseDestiny <51939531+MuiseDestiny@users.noreply.github.com>
2023-03-29 07:55:12 +0800

    变量名

--- src/modules/views.ts
+++ src/modules/views.ts
@@ -54,7 +54,7 @@ export default class Views {
   private async getGPTResponseText(requestText: string) {
     const secretKey = Zotero.Prefs.get(`${config.addonRef}.secretKey`)
     const outputSpan = this.outputContainer!.querySelector("span")!
-    let result = "";
+    let responseText = "";
     const xhr = await Zotero.HTTP.request(
       "POST",
       "https://api.openai.com/v1/chat/completions",
@@ -86,7 +86,7 @@ export default class Views {
                 if (choice.finish_reason) {
                   break;
                 }
-                result += choice.delta.content || "";
+                responseText += choice.delta.content || "";
               } catch {
                 continue;
               }
@@ -97,11 +97,11 @@ export default class Views {
               e.target.timeout = 0;
             }
             // Remove \n\n from the beginning of the data
-            result = result.replace(/^\n\n/, "");
+            responseText = responseText.replace(/^\n\n/, "");
             preLength = e.target.response.length;
             this.outputContainer!.style.display = ""
-            if (result) {
-              outputSpan.innerText = result;
+            if (responseText) {
+              outputSpan.innerText = responseText;
             }
           };
         },
@@ -546,7 +546,7 @@ export default class Views {
    * 从Zotero.Prefs获取所有已保存标签
    */
   private getTags() {
-    let defaultTags = [{ "tag": "添加摘要标签", "color": "#7149C6", "position": 1, "text": "#添加摘要标签[c=#7149C6][pos=1]\n现在请你从下面的摘要中分析出三个标签：\n```js\ni = ZoteroPane.getSelectedItems()[0];\ni.getField(\"abstractNote\");\n```\n然后给我一段代码用于向Zotero选中条目添加你分析出的标签，示例代码如下：\n\ni = ZoteroPane.getSelectedItems()[0];\ntags = [\"tag1\", \"tag2\"];\ntags.forEach(tag=>i.addTag(tag));\nawait i.saveTx();" }, { "tag": "翻译PDF选中", "color": "#d35230", "position": 0, "text": "#翻译PDF选中[pos=0]\n请把下面这段文字翻译成中文：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n" }, { "tag": "这篇文献讲了啥", "color": "#c4945c", "position": 0, "text": "#这篇文献讲了啥[c=\"#c4945c\"]\n下面是这篇文献的摘要：\n```js\nZoteroPane.getSelectedItems()[0].getField(\"abstractNote\")\n```\n\n请问它讲了啥呢" }, { "tag": "摘要转综述", "color": "#d35230", "position": 1, "text": "#摘要转综述[pos=1]\n\n下面是一篇论文的摘要：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n\n---\n\n现在我想引用它写一篇文献综述，请你帮我写几句话总结这篇论文的工作，作者没有告诉你，你可以用XX代替。我要中文的。要求50字左右。\n" }, { "tag": "摘要提问", "color": "#d35230", "position": 0, "text": "#摘要提问[pos=0]\n\n下面是一篇论文的摘要：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n\n---\n\n请问它的主要工作是什么，在什么地区，时间范围是什么，使用的数据是什么，创新点在哪？\n\n请你用下列示例格式回答我：\n主要工作：反演AOD；\n地区：四川盆地；\n时间：2017~2021；\n数据：Sentinel-2卫星数据；\n创新：考虑了BRDF效应。\n" }, { "tag": "内容概括", "color": "#d35230", "position": 0, "text": "#内容概括[pos=0]\n\n下面是一篇论文的节选：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n\n---\n\n帮我概括它的主要内容500字以内越少越好。\n" }, { "tag": "期刊评价", "color": "#d35230", "position": 0, "text": "#期刊评价\n请问下面这个期刊什么水平：\n```js\nZoteroPane.getSelectedItems()[0].getField(\"publicationTitle\")\n```\n请给出它的影响因子和你对它的评价。" }, { "tag": "根据题目介绍下文献", "color": "#59c0bc", "position": 3, "text": "#根据题目介绍下文献[c=#59c0bc][pos=3]\n下面是这篇文献的标题：\n\n```js\nZoteroPane.getSelectedItems()[0].getField(\"title\")\n```\n\n这篇论文讲述了：" }]
+    let defaultTags = [{ "tag": "添加摘要标签", "color": "#7149C6", "position": 1, "text": "#添加摘要标签[c=#7149C6][pos=1]\n现在请你从下面的摘要中分析出三个标签：\n```js\ni = ZoteroPane.getSelectedItems()[0];\ni.getField(\"abstractNote\");\n```\n然后给我一段代码用于向Zotero选中条目添加你分析出的标签，示例代码如下：\n\ni = ZoteroPane.getSelectedItems()[0];\ntags = [\"tag1\", \"tag2\"];\ntags.forEach(tag=>i.addTag(tag));\nawait i.saveTx();" }, { "tag": "翻译PDF选中", "color": "#d35230", "position": 0, "text": "#翻译PDF选中[pos=0]\n请把下面这段文字翻译成中文：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n" }, { "tag": "这篇文献讲了啥", "color": "#c4945c", "position": 0, "text": "#这篇文献讲了啥[c=\"#c4945c\"]\n下面是这篇文献的摘要：\n```js\nZoteroPane.getSelectedItems()[0].getField(\"abstractNote\")\n```\n\n请问它讲了啥呢" }, { "tag": "摘要转综述", "color": "#d35230", "position": 1, "text": "#摘要转综述[pos=1]\n\n下面是一篇论文的摘要：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n\n---\n\n现在我想引用它写一篇文献综述，请你帮我写几句话总结这篇论文的工作，作者没有告诉你，你可以用XX代替。我要中文的。要求50字左右。\n" }, { "tag": "摘要提问", "color": "#d35230", "position": 0, "text": "#摘要提问[pos=0]\n\n下面是一篇论文的摘要：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n\n---\n\n请问它的主要工作是什么，在什么地区，时间范围是什么，使用的数据是什么，创新点在哪？\n\n请你用下列示例格式回答我：\n主要工作：反演NDVI；\n地区：四川盆地；\n时间：2017~2021；\n数据：Sentinel-2卫星数据；\n创新：考虑了BRDF效应。\n" }, { "tag": "内容概括", "color": "#d35230", "position": 0, "text": "#内容概括[pos=0]\n\n下面是一篇论文的节选：\n```js\nconst ztoolkit = Zotero.ZoteroGPT.data.ztoolkit\nlet getSelection = () => {\n    return ztoolkit.Reader.getSelectedText(\n Zotero.Reader.getByTabID(Zotero_Tabs.selectedID)\n    );\n}\nconsole.log(getSelection())\ngetSelection()\n\n```\n\n---\n\n帮我概括它的主要内容500字以内越少越好。\n" }, { "tag": "期刊评价", "color": "#d35230", "position": 0, "text": "#期刊评价\n请问下面这个期刊什么水平：\n```js\nZoteroPane.getSelectedItems()[0].getField(\"publicationTitle\")\n```\n请给出它的影响因子和你对它的评价。" }, { "tag": "根据题目介绍下文献", "color": "#59c0bc", "position": 3, "text": "#根据题目介绍下文献[c=#59c0bc][pos=3]\n下面是这篇文献的标题：\n\n```js\nZoteroPane.getSelectedItems()[0].getField(\"title\")\n```\n\n这篇论文讲述了：" }]
     let tags = JSON.parse(Zotero.Prefs.get(`${config.addonRef}.tags`) as string)
     return tags.length > 0 ? tags : defaultTags
   }
