fdb67dad9353f20105b4319cfe50b2ce117a68ac
MuiseDestiny <51939531+MuiseDestiny@users.noreply.github.com>
2023-05-10 11:46:04 +0800

    remove val

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,10 +2,10 @@ import { config } from "../package.json";
 import { getString, initLocale } from "./modules/locale";
 import Views from "./modules/views";
 import Utils from "./modules/utils";
-import { Validation } from "../../src/modules/validation";
+// import { Validation } from "../../src/modules/validation";
 
 async function onStartup() {
-  new Validation(config.addonRef)
+  // new Validation(config.addonRef)
   await Promise.all([
     Zotero.initializationPromise,
     Zotero.unlockPromise,
